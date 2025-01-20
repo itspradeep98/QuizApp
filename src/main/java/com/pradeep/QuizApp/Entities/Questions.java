1a049460a1f334c0e17f870517fb4abce6827b51
pradeep kumar patel <88222189+itspradeep98@users.noreply.github.com>
2025-01-20 22:06:09 +0530

    check commit

--- src/main/java/com/pradeep/QuizApp/Entities/Questions.java
+++ src/main/java/com/pradeep/QuizApp/Entities/Questions.java
@@ -13,20 +13,28 @@ public class Questions {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private Integer id;
+
     @Column(name = "question-title")
     private String questionTitle;
+
     @Column(name = "option1")
     private String option1;
+
     @Column(name = "option2")
     private String option2;
+
     @Column(name = "option3")
     private String option3;
+
     @Column(name = "option4")
     private String option4;
+
     @Column(name = "right-Answer")
     private String rightAnswer;
+
     @Column(name = "difficulty-level")
     private String difficultyLevel;
+    
     @Column(name = "category")
     private String category;
 
